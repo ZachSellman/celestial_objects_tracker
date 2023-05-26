1fe628ba73068b41fa9cdea3fcf3bc88b932a870
ZachSellman <zachpython21@gmail.com>
2023-05-26 18:02:27 -0500

    get_planets now effectively generates visible planets

--- main.py
+++ main.py
@@ -1,43 +1,88 @@
 """Python app to track multiple "Celestial Bodies" such as planets, the ISS, etc. and alert user when they are visible from their location."""
 
-import requests
-import sys
-from os import getenv
-from dotenv import load_dotenv
 from datetime import datetime as dt
+from os import getenv
+import sys
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from dotenv import load_dotenv
+import requests
+
 
 load_dotenv()
 
 MY_LAT = 44.97
 MY_LONG = -93.26
-LOCATION = "Minneapolis"
+LOCATION = getenv("LOCATION")
 WEATHER_API_KEY = getenv("WEATHER_API_KEY")
-MY_TIMEZONE = "CST"
-CURRENT_TIME = dt.time.now()
+MY_TIMEZONE = getenv("MY_TIMEZONE")
+CURRENT_TIME = dt.now().strftime("%H:%M:%S")
+SELENIUM_PATH = getenv("SELENIUM_PATH")
 
 
 def main():
     cloud = check_weather()
     if cloud > 60:
-        fail(0)
+        # fail(0)
+        print("It's kinda cloudy")
 
     dusk, dawn = check_darkness()
     if not CURRENT_TIME < dawn or not CURRENT_TIME > dusk:
-        fail(1)
+        # fail(1)
+        print("It's currently too bright out!")
 
     objects = get_objects()
+    print(f"These objects are currently visible: {objects}")
 
 
 def get_objects():
     iss_status = get_iss()
     planets = get_planets()
+    if iss_status is True:
+        planets.append("International Space Station")
+
+    return planets
 
 
 def get_planets():
-    "Will utilize selenium to scrape which planets should be visible at night"
+    service = Service(executable_path=SELENIUM_PATH)
+    options = Options()
+    web_url = "https://www.timeanddate.com/astronomy/night/"
+
+    # comment this add_argument("--headless") to show browser window
+    options.add_argument("--headless")
+    options.add_argument("--disable-3d-apis")
+    options.add_argument("--window-size=1920,1200")
+    driver = webdriver.Chrome(options=options, service=service)
+    driver.get(web_url)
+
+    # via XPATH:
+    planet_table = driver.find_element(
+        by=By.XPATH,
+        value="/html/body/div[5]/main/article/section[1]/div[2]/table/tbody",
+    )
+
+    planets_dict = {}
+
+    for row in planet_table.find_elements(by=By.CSS_SELECTOR, value="tr"):
+        planet = row.find_element(by=By.CSS_SELECTOR, value="th")
+        time = row.find_element(by=By.CSS_SELECTOR, value="td")
+        planets_dict[planet.text.strip().replace(":", "")] = time.text.strip()
+
+    visible_planets = []
+    for planet, time in planets_dict.items():
+        hour = time[-8:-3].strip()
+
+        # Need to add 12 to change format to 24 hr time for comparison
+        if time[-2:] == "pm":
+            hour = str(int(time[-8:-6].strip()) + 12) + time[-6:-3]
+
+        if hour < CURRENT_TIME:
+            visible_planets.append(planet)
+
+    return visible_planets
 
 
 def get_iss():
@@ -51,21 +96,16 @@ def get_iss():
         data["iss_position"]["longitude"],
     )
 
-    if iss_lat in range((MY_LAT - 5), (MY_LAT + 5)) and iss_long in range(
-        (MY_LONG - 5), (MY_LONG + 5)
-    ):
-        return True
-    else:
-        return False
+    return bool(MY_LAT - 5) < float(iss_lat) < (MY_LAT + 5) and (MY_LONG - 5) < float(
+        iss_long
+    ) < (MY_LONG + 5)
 
 
 def check_darkness():
     """Checks when it will be dusk"""
     today = f"{str(dt.now().year)}-{str(dt.now().month)}-{str(dt.now().day)}"
-    params = {"lat": MY_LAT, "lng": MY_LONG, "date": today, "timezone": MY_TIMEZONE}
     response = requests.get(
-        url="https://api.sunrisesunset.io/json",
-        params=params,
+        url=f"https://api.sunrisesunset.io/json?lat={MY_LAT}&lng={MY_LONG}&date={today}&timezone={MY_TIMEZONE}",
         timeout=1,
     )
 
@@ -87,17 +127,17 @@ def check_weather():
     return response.json()["current"]["cloud"]
 
 
-def check_field_of_view(obj_dict):
-    "Checks the visibility of fast-moving objects by comparing to user's MY_LAT and MY_LONG coordinates"
-    visible_objects = []
-    for obj in obj_dict:
-        obj_lat, obj_long = obj_dict[obj][0], obj_dict[obj][1]
-        if (MY_LAT - 5) <= float(obj_lat) <= (MY_LAT + 5) and (MY_LONG - 5) <= float(
-            obj_long
-        ) <= (MY_LONG + 5):
-            visible_objects.append(obj)
+# def check_field_of_view(obj_dict):
+#     "Checks the visibility of fast-moving objects by comparing to user's MY_LAT and MY_LONG coordinates"
+#     visible_objects = []
+#     for obj in obj_dict:
+#         obj_lat, obj_long = obj_dict[obj][0], obj_dict[obj][1]
+#         if (MY_LAT - 5) <= float(obj_lat) <= (MY_LAT + 5) and (MY_LONG - 5) <= float(
+#             obj_long
+#         ) <= (MY_LONG + 5):
+#             visible_objects.append(obj)
 
-    return visible_objects
+#     return visible_objects
 
 
 def fail(_x):
