8364a216d1baf75076dd96300fa04a85aa7055da
ZachSellman <zachsellman21@gmail.com>
2023-06-23 12:55:33 -0500

    added links to base pages supplied by HTML5

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.homepage, name="index"),
     path("base", views.base, name="base"),
+    path("leftbar", views.leftbar, name="leftbar"),
+    path("rightbar", views.rightbar, name="rightbar"),
+    path("nobar", views.nobar, name="nobar"),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -9,3 +9,15 @@ def homepage(request):
 
 def base(request):
     return render(request, "tracker/base.html")
+
+
+def leftbar(request):
+    return render(request, "tracker/left-sidebar.html")
+
+
+def rightbar(request):
+    return render(request, "tracker/right-sidebar.html")
+
+
+def nobar(request):
+    return render(request, "tracker/no-sidebar.html")
